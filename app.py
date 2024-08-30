8dbfa056f5f3a72c9a948c7b3a4eff222a8d75ed
Vaithiyanathan K <107228405+vkmatrix@users.noreply.github.com>
2024-08-30 19:40:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def home():
     return "Hello"
 print(f.__version__)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
 

858a0648be1f72aa6056c962ae9efa529f006a87
Vaithiyanathan K <107228405+vkmatrix@users.noreply.github.com>
2024-08-30 20:14:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 import flask as f
 import gunicorn
 app = Flask(__name__)
 
 @app.route('/')
+def home1():
+    return render_template("index.html")
+@app.route('/greet')
 def home():
     return "Hello"
 if __name__ == "__main__":
